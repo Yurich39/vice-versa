d7a0c76e3e8db9af1ba5e47165a29a0df6d237a6
Yurich39 <tribunskiy16@yahoo.com>
2023-03-30 10:52:16 +0500

    Added words count feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,4 +8,4 @@ def reverse(request):
 	# reauest это параметре, содержащий в себе очень много инфы.
 	user_text = request.GET['usertext']
 	#print(user_text) #такой текст выводится в терминале
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':user_text[::-1]}) #теперь в файле reverse.html мы можешь обращаться к ключу 'usertext'	
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':user_text[::-1], 'num_of_words':len(user_text.split())}) #теперь в файле reverse.html мы можешь обращаться к ключу 'usertext'	
